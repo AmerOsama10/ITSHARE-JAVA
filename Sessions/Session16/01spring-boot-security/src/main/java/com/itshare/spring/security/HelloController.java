5333d3eef02f35a1a681947f0f7369330809f828
AmerOsama10 <amerosama.jr@gmail.com>
2023-11-11 01:46:37 +0200

    edited rest

--- Sessions/Session16/01spring-boot-security/src/main/java/com/itshare/spring/security/HelloController.java
+++ Sessions/Session16/01spring-boot-security/src/main/java/com/itshare/spring/security/HelloController.java
@@ -8,16 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-//application flow
-// 1- request mapping(/showForm)
-// 2- controller
-// 3- view -> hello.html
-
 @Controller
 public class HelloController{
 	
 	@RequestMapping("/showForm")
 	public String showForm() {
+		
 		return "hello-form";
 	}
 	
